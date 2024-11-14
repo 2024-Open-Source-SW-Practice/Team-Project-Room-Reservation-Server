08bb5b02b50b0cb4b8d73fc4a1639df77a338b55
jae woo Jung <awfjol2008@gmail.com>
2024-11-14 15:49:06 +0900

    feat: setup project

---
+++ src/main/java/com/kyonggi/reservation/TeamProjectRoomReservationApplication.java
@@ -0,0 +1,13 @@
+package com.kyonggi.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectRoomReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProjectRoomReservationApplication.class, args);
+    }
+
+}

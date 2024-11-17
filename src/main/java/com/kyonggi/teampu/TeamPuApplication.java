a7e7fe2cd6568ea1cbd38d533dfa41bd15726d91
jae woo Jung <awfjol2008@gmail.com>
2024-11-18 02:25:19 +0900

    refactor: 프로젝트명 변경 사항을 각 클래스에 적용

--- src/main/java/com/kyonggi/teampu/TeamPuApplication.java
+++ src/main/java/com/kyonggi/teampu/TeamPuApplication.java
@@ -1,13 +1,13 @@
-package com.kyonggi.reservation;
+package com.kyonggi.teampu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TeamProjectRoomReservationApplication {
+public class TeamPuApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TeamProjectRoomReservationApplication.class, args);
+        SpringApplication.run(TeamPuApplication.class, args);
     }
 
 }

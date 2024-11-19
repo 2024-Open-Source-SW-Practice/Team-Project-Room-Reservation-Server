81787d11ce69b4537ea03c1e36348e3a53fc3d61
jae woo Jung <awfjol2008@gmail.com>
2024-11-19 16:43:21 +0900

    feat: 공통 응답 모델 구현

---
+++ src/main/java/com/kyonggi/teampu/global/response/ApiResponse.java
@@ -0,0 +1,46 @@
+package com.kyonggi.teampu.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private Status status;
+
+    private Metadata metadata;
+
+    private List<T> results;
+
+    public static <T> ApiResponse<T> ok(List<T> results) {
+        return new ApiResponse<>(
+                new Status(HttpStatus.OK, "OK"),
+                new Metadata(results.size()),
+                results
+        );
+    }
+
+    public static <T> ApiResponse<T> ok(T body) {
+        return new ApiResponse<>(
+                new Status(HttpStatus.OK, "OK"),
+                new Metadata(1),
+                List.of(body)
+        );
+    }
+
+    @Getter
+    @AllArgsConstructor
+    private static class Metadata {
+        private int resultCount = 0;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    private static class Status {
+        private HttpStatus httpStatus;
+        private String message;
+    }
+}

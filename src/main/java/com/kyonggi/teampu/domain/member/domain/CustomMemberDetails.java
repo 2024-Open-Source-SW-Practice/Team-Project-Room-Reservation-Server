6b41df3f2580f428f8fad69888ea7a6c97f828ca
jae woo Jung <awfjol2008@gmail.com>
2024-11-21 21:28:55 +0900

    feat: CustomMemberDetails 클래스 구현
    스프링 시큐리티에서 관리하는 회원 클래스 구현

---
+++ src/main/java/com/kyonggi/teampu/domain/member/domain/CustomMemberDetails.java
@@ -0,0 +1,33 @@
+package com.kyonggi.teampu.domain.member.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomMemberDetails implements UserDetails {
+    private final Member member;
+
+    public CustomMemberDetails(Member member) {
+        this.member = member;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+}

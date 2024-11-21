bc30456a94119102dda4b7b2774b54a6e0de7cd6
jae woo Jung <awfjol2008@gmail.com>
2024-11-21 21:27:01 +0900

    feat: 회원 조회 시 Optional 타입을 반환하도록 적용

--- src/main/java/com/kyonggi/teampu/domain/member/repository/MemberRepository.java
+++ src/main/java/com/kyonggi/teampu/domain/member/repository/MemberRepository.java
@@ -3,9 +3,14 @@ package com.kyonggi.teampu.domain.member.repository;
 import com.kyonggi.teampu.domain.member.domain.Member;
 import org.springframework.data.repository.Repository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends Repository<Member, Long> {
     Member save(Member member);
-    Member findByLoginId(String loginId);
-    Member findById(Long id);
+
+    Optional<Member> findByLoginId(String loginId);
+
+    Optional<Member> findById(Long id);
+
     void delete(Member member);
 }

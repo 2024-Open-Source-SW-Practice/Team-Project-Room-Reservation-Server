fa262d5a77680c0d10275966b0fed5611f3d6586
jae woo Jung <awfjol2008@gmail.com>
2024-11-21 17:13:49 +0900

    feat: 회원가입 API 구현

--- src/main/java/com/kyonggi/teampu/domain/member/controller/MemberController.java
+++ src/main/java/com/kyonggi/teampu/domain/member/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.kyonggi.teampu.domain.member.controller;
 
+import com.kyonggi.teampu.domain.member.dto.JoinRequest;
 import com.kyonggi.teampu.domain.member.service.MemberService;
+import com.kyonggi.teampu.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api")
 public class MemberController {
     private final MemberService memberService;
+
+    @PostMapping("/join")
+    public ApiResponse<Void> join(@RequestBody JoinRequest request) {
+        memberService.join(request);
+
+        return ApiResponse.noContent();
+    }
 }

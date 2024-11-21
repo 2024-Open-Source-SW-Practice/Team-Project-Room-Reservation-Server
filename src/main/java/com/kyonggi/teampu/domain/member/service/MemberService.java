a06539c6f932e74190232551a505b5bd1e0df1fe
jae woo Jung <awfjol2008@gmail.com>
2024-11-21 16:28:39 +0900

    feat: 회원가입 기능 구현

--- src/main/java/com/kyonggi/teampu/domain/member/service/MemberService.java
+++ src/main/java/com/kyonggi/teampu/domain/member/service/MemberService.java
@@ -1,5 +1,8 @@
 package com.kyonggi.teampu.domain.member.service;
 
+import com.kyonggi.teampu.domain.member.domain.Member;
+import com.kyonggi.teampu.domain.member.domain.MemberType;
+import com.kyonggi.teampu.domain.member.dto.JoinRequest;
 import com.kyonggi.teampu.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +13,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+
+    public void join(JoinRequest request) {
+        Member member = Member.builder()
+                .loginId(request.getLoginId())
+                .password(request.getPassword())
+                .name(request.getName())
+                .college(request.getCollege())
+                .department(request.getDepartment())
+                .phoneNumber(request.getPhoneNumber())
+                .email(request.getEmail())
+                .type(MemberType.findByName(request.getType()))
+                .build();
+
+        memberRepository.save(member);
+    }
 }

9a61d1d433f96839d75f9d0473ea044fb529573b
jae woo Jung <awfjol2008@gmail.com>
2024-11-22 01:56:13 +0900

    feat: CustomEntryFilter 구현

---
+++ src/main/java/com/kyonggi/teampu/global/filter/CustomEntryPoint.java
@@ -0,0 +1,39 @@
+package com.kyonggi.teampu.global.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kyonggi.teampu.global.exception.ErrorCode;
+import com.kyonggi.teampu.global.response.ApiResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static com.kyonggi.teampu.global.exception.ErrorCode.*;
+
+@Slf4j
+@Component
+public class CustomEntryPoint implements AuthenticationEntryPoint {
+	@Override
+	public void commence(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			AuthenticationException e
+	) throws IOException, ServletException {
+		createAPIResponse(response, MEMBER_NOT_AUTHENTICATED);
+	}
+
+	private void createAPIResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+		ApiResponse<Object> apiResponse = ApiResponse.exception(errorCode);
+		response.setStatus(errorCode.getHttpStatus().value());
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setCharacterEncoding("UTF-8");
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.writeValue(response.getWriter(), apiResponse);
+	}
+}

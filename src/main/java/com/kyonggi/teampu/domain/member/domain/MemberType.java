fec88489838ed09d99a7c3da2e1a356dffc614da
jae woo Jung <awfjol2008@gmail.com>
2024-11-21 16:27:14 +0900

    feat: MemberType Enum에 name 필드 추가

--- src/main/java/com/kyonggi/teampu/domain/member/domain/MemberType.java
+++ src/main/java/com/kyonggi/teampu/domain/member/domain/MemberType.java
@@ -1,7 +1,13 @@
 package com.kyonggi.teampu.domain.member.domain;
 
 public enum MemberType {
-    UNDERGRADUATE,
-    GRADUATE,
-    PROFESSOR
+    UNDERGRADUATE("학부생"),
+    GRADUATE("대학원생"),
+    PROFESSOR("교수");
+
+    MemberType(String name) {
+        this.name = name;
+    }
+
+    private final String name;
 }

9d591429c5b6286ec4e93947f19e7d2203c7998d
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2024-11-23 14:48:46 +0900

    feat: Member, Application 데이터 삽입 SQL문 추가

---
+++ src/main/resources/db/Add_Member.sql
@@ -0,0 +1,11 @@
+INSERT INTO Members (IS_ADMIN, MEMBER_ID, COLLEGE, DEPARTMENT, EMAIL, LOGIN_ID, NAME, PASSWORD, PHONE_NUMBER, TYPE) VALUES
+                        (true, 1, 'Business', 'Finance', 'user1@example.com', 'user1', 'Member 1', 'Password1', '010-3113-1028', 'GRADUATE'),
+                        (true, 2, 'Science', 'Finance', 'user2@example.com', 'user2', 'Member 2', 'Password2', '010-2982-6360', 'GRADUATE'),
+                        (true, 3, 'Arts', 'English', 'user3@example.com', 'user3', 'Member 3', 'Password3', '010-2231-6136', 'GRADUATE'),
+                        (false, 4, 'Science', 'Biology', 'user4@example.com', 'user4', 'Member 4', 'Password4', '010-9358-9100', 'GRADUATE'),
+                        (false, 5, 'Science', 'Biology', 'user5@example.com', 'user5', 'Member 5', 'Password5', '010-9257-7809', 'UNDERGRADUATE'),
+                        (true, 6, 'Science', 'Finance', 'user6@example.com', 'user6', 'Member 6', 'Password6', '010-3289-8247', 'UNDERGRADUATE'),
+                        (false, 7, 'Engineering', 'Computer Science', 'user7@example.com', 'user7', 'Member 7', 'Password7', '010-3282-3349', 'UNDERGRADUATE'),
+                        (true, 8, 'Arts', 'English', 'user8@example.com', 'user8', 'Member 8', 'Password8', '010-4485-2029', 'PROFESSOR'),
+                        (false, 9, 'Arts', 'English', 'user9@example.com', 'user9', 'Member 9', 'Password9', '010-7227-2136', 'PROFESSOR'),
+                        (false, 10, 'Business', 'English', 'user10@example.com', 'user10', 'Member 10', 'Password10', '010-7903-9891', 'PROFESSOR');

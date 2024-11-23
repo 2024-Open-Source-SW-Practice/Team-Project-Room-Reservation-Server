2cdb954ab7df6c3035581069363c9fd520c7bb3d
ahyeonkong <kimah0403@gmail.com>
2024-11-23 15:38:06 +0900

    refactor: ApplicationController 예외 반환 로직 개선
    * ResponseEntity를 ApiResponse로 통일
    * ApiResponse.exception을 활용해 응답

--- src/main/java/com/kyonggi/teampu/domain/application/controller/ApplicationController.java
+++ src/main/java/com/kyonggi/teampu/domain/application/controller/ApplicationController.java
@@ -3,10 +3,9 @@ package com.kyonggi.teampu.domain.application.controller;
 import com.kyonggi.teampu.domain.application.dto.ApplicationRequest;
 import com.kyonggi.teampu.domain.application.dto.ApplicationResponse;
 import com.kyonggi.teampu.domain.application.service.ApplicationService;
+import com.kyonggi.teampu.global.exception.ErrorCode;
 import com.kyonggi.teampu.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,31 +18,20 @@ public class ApplicationController {
     private final ApplicationService applicationService;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<ApplicationResponse>> createApplication(@RequestBody ApplicationRequest applicationRequest) {
+    public ApiResponse<ApplicationResponse> createApplication(@RequestBody ApplicationRequest applicationRequest) {
         // 400 에러 처리
-        if (applicationRequest.getMemberId() == null || applicationRequest.getStartTime() == null || applicationRequest.getEndTime() == null) {
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body(new ApiResponse<>(
-                            new ApiResponse.Status(HttpStatus.BAD_REQUEST, "필수 값이 누락되었습니다"),
-                            null
-                    ));
+        if (applicationRequest.getMemberId() == null ||
+                applicationRequest.getStartTime() == null ||
+                applicationRequest.getEndTime() == null) {
+            return ApiResponse.exception(ErrorCode.INVALID_REQUEST);
         }
-
         try {
-            ApplicationResponse applicationResponse = applicationService.createApplication(applicationRequest);
-            return ResponseEntity.ok(new ApiResponse<>(
-                    new ApiResponse.Status(HttpStatus.CREATED, "신청이 완료되었습니다"),
-                    applicationResponse
-            ));
-        } catch (IllegalArgumentException e) {
+            // 201 응답 처리
+            ApplicationResponse aplApplicationResponse = applicationService.createApplication(applicationRequest);
+            return ApiResponse.ok(aplApplicationResponse);
+        }catch (IllegalArgumentException e){
             // 401 에러 처리
-            return ResponseEntity
-                    .status(HttpStatus.UNAUTHORIZED)
-                    .body(new ApiResponse<>(
-                            new ApiResponse.Status(HttpStatus.UNAUTHORIZED, "권한이 없습니다"),
-                            null
-                    ));
+            return ApiResponse.exception(ErrorCode.NOT_ENOUGH_PERMISSION);
         }
     }
 }

a1c780764ad33daa1cb41f64d40ab4db0291e665
ahyeonkong <kimah0403@gmail.com>
2024-11-24 16:53:50 +0900

    fix: MyPageService의 잘못된 import 변경

--- src/main/java/com/kyonggi/teampu/domain/myPage/service/MyPageService.java
+++ src/main/java/com/kyonggi/teampu/domain/myPage/service/MyPageService.java
@@ -3,7 +3,7 @@ package com.kyonggi.teampu.domain.myPage.service;
 import com.kyonggi.teampu.domain.member.domain.Member;
 import com.kyonggi.teampu.domain.myPage.dto.MyPageRequest;
 import com.kyonggi.teampu.domain.myPage.dto.MyPageResponse.MyPageDTO;
-import com.kyonggi.teampu.domain.myPage.repository.MemberRepository;
+import com.kyonggi.teampu.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;

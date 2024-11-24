3e889daddbc812f789124f1146add05926cb45b9
ahyeonkong <kimah0403@gmail.com>
2024-11-24 16:55:52 +0900

    fix: 공동 참여자 정보 변수 수정 및 리스트 추가

--- src/main/java/com/kyonggi/teampu/domain/member/dto/CoParticipantRequest.java
+++ src/main/java/com/kyonggi/teampu/domain/member/dto/CoParticipantRequest.java
@@ -4,12 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @Builder
 @AllArgsConstructor
 public class CoParticipantRequest {
-    private final Long userId;
-    private final String loginId;
-    private final String name;
+    // CoParticipantRequest: 공동 참여자 정보를 서버로 전송할 때 사용하는 객체
+    private String name; // 이름
+    private String phoneNumber; // 전화번호
+
+    // CoParticipantRequest에서 이름과 전화번호를 리스트로 변환하려면, CoParticipantRequest 클래스에 변환 메서드를 추가
+    public static List<String> toNameList(List<CoParticipantRequest> coParticipantRequests) {
+        return coParticipantRequests.stream()
+                .map(CoParticipantRequest::getName)
+                .toList();
+    }
 
 }

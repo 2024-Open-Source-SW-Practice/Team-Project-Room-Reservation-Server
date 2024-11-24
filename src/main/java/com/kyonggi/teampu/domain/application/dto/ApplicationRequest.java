6845361a064cfb1bbeead05713f59915b0dfbcbf
ahyeonkong <kimah0403@gmail.com>
2024-11-24 16:50:49 +0900

    refactor: ApplicationRequest에서 날짜 변수 수정 및 builder 패턴 제거

--- src/main/java/com/kyonggi/teampu/domain/application/dto/ApplicationRequest.java
+++ src/main/java/com/kyonggi/teampu/domain/application/dto/ApplicationRequest.java
@@ -1,34 +1,26 @@
 package com.kyonggi.teampu.domain.application.dto;
-
-import com.kyonggi.teampu.domain.application.domain.Application;
-import com.kyonggi.teampu.domain.application.domain.ApplicationStatus;
-import com.kyonggi.teampu.domain.member.domain.Member;
+import com.kyonggi.teampu.domain.member.dto.CoParticipantRequest;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
-@Getter
-@NoArgsConstructor
-public class ApplicationRequest {
-    private Integer participantCount;
-    private List<String> coParticipantNames;
-    private Boolean privacyAgreement;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
+/**
+ * 예약 제출 페이지에서
+ * 사용자의 정보(이름, 학번, 전화번호, 이메일)를 자동으로 받아옴 >> 로그인 정보에서 가져옴
+ * 날짜 >> 사용자가 직접 입력
+ * 공동 참여자 목록(이름, 전화번호) >> 사용자가 직접 입력
+ * 인원 >> 공동 참여자 목록 + 1으로 신청자 포함한 사용 인원 자동 계산
+ * 개인정보 동의 여부 >> 사용자가 직접 체크
+ */
 
-    public Application toEntity(Member member) {
-        return Application.builder()
-                .member(member)
-                .participantCount(participantCount)
-                .coParticipantNames(coParticipantNames)
-                .privacyAgreement(privacyAgreement)
-                .startTime(startTime)
-                .endTime(endTime)
-                .appliedDate(LocalDate.now())
-                .status(ApplicationStatus.PENDING)
-                .build();
-    }
+@Getter
+@AllArgsConstructor
+public class ApplicationRequest {
+    // 로그인 정보를 통해 이름, 학번, 전화번호, 이메일 받아옴
+    private LocalDate date; // 날짜
+    private List<CoParticipantRequest> coParticipants; // 공동 참여자 목록 (이름, 전화번호)
+    private Boolean privacyAgreement; // 개인정보 동의 여부
+
 }

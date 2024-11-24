06b219e47e90961b9de5c6b2ca9a3c33a78aa225
ahyeonkong <kimah0403@gmail.com>
2024-11-24 23:29:04 +0900

    refactor: ApplicationStatus의 enum 값을 json으로 직렬화/역직렬화

--- src/main/java/com/kyonggi/teampu/domain/application/domain/ApplicationStatus.java
+++ src/main/java/com/kyonggi/teampu/domain/application/domain/ApplicationStatus.java
@@ -1,7 +1,21 @@
 package com.kyonggi.teampu.domain.application.domain;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ApplicationStatus {
-    PENDING,
-    APPROVED,
-    REJECTED
+    PENDING("pending"),
+    APPROVED("approved"),
+    REJECTED("rejected");
+
+    private final String code;
+
+    ApplicationStatus(String code) {
+        this.code = code;
+    }
+
+    @JsonValue
+    public String getCode() {
+        return this.code;
+    }
+
 }

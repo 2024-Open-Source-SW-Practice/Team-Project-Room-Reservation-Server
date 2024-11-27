22625f677b0d517ec0411a2af877c68a20c33e17
ahyeonkong <kimah0403@gmail.com>
2024-11-27 15:41:55 +0900

    refactor: @EntityGraph를 사용하여 fetch join 기능 개선

--- src/main/java/com/kyonggi/teampu/domain/application/repository/ApplicationRepository.java
+++ src/main/java/com/kyonggi/teampu/domain/application/repository/ApplicationRepository.java
@@ -1,11 +1,17 @@
 package com.kyonggi.teampu.domain.application.repository;
 
 import com.kyonggi.teampu.domain.application.domain.Application;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
     List<Application> findByAppliedDateBetween(LocalDate startDate, LocalDate endDate);
+
+    @EntityGraph(attributePaths = {"member"}) // fetch join 사용
+    Optional<Application> findById(Long id);
+
 }

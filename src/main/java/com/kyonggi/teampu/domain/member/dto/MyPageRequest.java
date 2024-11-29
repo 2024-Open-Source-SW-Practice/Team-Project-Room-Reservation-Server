a9fcec1f263682ba7c1e3321ba43756c11153fd3
jae woo Jung <awfjol2008@gmail.com>
2024-11-29 20:53:51 +0900

    feat: 테스트 편의를 위한 DTO 생성자 추가

--- src/main/java/com/kyonggi/teampu/domain/member/dto/MyPageRequest.java
+++ src/main/java/com/kyonggi/teampu/domain/member/dto/MyPageRequest.java
@@ -1,10 +1,12 @@
 package com.kyonggi.teampu.domain.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 public class MyPageRequest {
 
     @Getter
+    @AllArgsConstructor
     public static class UpdateProfileDTO {
         private String name;
         private String phoneNumber;

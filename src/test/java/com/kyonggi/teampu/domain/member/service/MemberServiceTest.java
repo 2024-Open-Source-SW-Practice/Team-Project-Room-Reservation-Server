fd778edf2e243b695fa4e6d93f9af86c9bfa038d
jae woo Jung <awfjol2008@gmail.com>
2024-11-30 15:48:57 +0900

    fix: 각 테스트가 테스트용 application.yml을 사용하도록 변경

--- src/test/java/com/kyonggi/teampu/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/kyonggi/teampu/domain/member/service/MemberServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static com.kyonggi.teampu.global.exception.ErrorCode.MEMBER_NOT_FOUND;
 import static com.kyonggi.teampu.global.exception.ErrorCode.MEMBER_NOT_FOUND_BY_LOGIN_ID;
@@ -21,6 +22,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 @DisplayName("MemberService 테스트")
 class MemberServiceTest {
     @Autowired

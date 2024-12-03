373c18c9688a9b51d442b623fc810bffd00c3ac8
ahyeonkong <kimah0403@gmail.com>
2024-12-03 17:11:32 +0900

    test: 테스트 환경 설정 임시 비활성화

--- src/test/java/com/kyonggi/teampu/TeamPuApplicationTests.java
+++ src/test/java/com/kyonggi/teampu/TeamPuApplicationTests.java
@@ -1,15 +1,15 @@
-package com.kyonggi.teampu;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-//@SpringBootTest 임시로 주석 처리
-@ActiveProfiles("test") // application-test.yml 파일의 설정을 로드
-class TeamPuApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.kyonggi.teampu;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+////@SpringBootTest 임시로 주석 처리
+//@ActiveProfiles("test") // application-test.yml 파일의 설정을 로드
+//class TeamPuApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}

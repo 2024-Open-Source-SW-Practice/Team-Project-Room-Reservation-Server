06d2017cfb36d4ccac2412f26c28233622b11dc1
ahyeonkong <kimah0403@gmail.com>
2024-12-03 16:23:14 +0900

    fix: 불필요한 어노테이션 제거

--- src/test/java/com/kyonggi/teampu/TeamPuApplicationTests.java
+++ src/test/java/com/kyonggi/teampu/TeamPuApplicationTests.java
@@ -1,12 +1,10 @@
 package com.kyonggi.teampu;
 
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@Transactional
+//@SpringBootTest 임시로 주석 처리
 @ActiveProfiles("test") // application-test.yml 파일의 설정을 로드
 class TeamPuApplicationTests {
 

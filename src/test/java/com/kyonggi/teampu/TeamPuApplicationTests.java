3415a8f47e258d0e53a8635ae4906ccf2ec9c44d
ahyeonkong <kimah0403@gmail.com>
2024-12-03 11:18:14 +0900

    fix: application-test.yml 파일의 설정 로드 어노테이션 추가

--- src/test/java/com/kyonggi/teampu/TeamPuApplicationTests.java
+++ src/test/java/com/kyonggi/teampu/TeamPuApplicationTests.java
@@ -3,9 +3,11 @@ package com.kyonggi.teampu;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test") // application-test.yml 파일의 설정을 로드
 class TeamPuApplicationTests {
 
     @Test
